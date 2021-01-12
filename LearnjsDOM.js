dcfe4f95afb64e557c8247deee49c9b5edb1cc1c
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2021-01-12 23:17:02 +0700

    DOM Selection Part 2

--- LearnjsDOM.js
+++ LearnjsDOM.js
@@ -21,3 +21,23 @@ h1.style.color = 'orange';
 const p1 = document.getElementsByClassName('p1') [0];
 p1.style.fontSize = '20px';
 p1.innerHTML = 'paragraf pertama';
+
+// document.querySelector() -> element
+const p4 = document.querySelector('#secdua p');
+p4.style.color = 'yellow';
+p4.innerHTML = 'ini dari javascript'
+
+const li3 = document.querySelector('section#secdua ul li:nth-child(3)');
+li3.style.background = 'black';
+li3.style.color = 'red';
+li3.style.textAlign = 'center';
+
+// document,querySelectorAll() -> Nodelist
+// const p = document.querySelectorAll('p');
+// for( let i = 0; i < p.length; i++ ) {
+   // p[i].style.fontFamily = 'timesNewRoman';
+// }
+
+// const sectionsecdua = document.getElementById('secdua');
+// const p4 = sectionsecdua.querySelector('p');
+// p4.style.textAlign = 'center';
