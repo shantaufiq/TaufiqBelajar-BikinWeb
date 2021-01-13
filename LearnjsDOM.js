0d1cda1fb8e047edbd4d4540d9b4a0b63ed24b95
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2021-01-13 15:01:42 +0700

    DOM Manipulation Part 1

--- LearnjsDOM.js
+++ LearnjsDOM.js
@@ -1,38 +1,38 @@
 // Dom Selection
-// document.getElementById() -> element
-const judul = document.getElementById('judul')
-judul.style.color = 'orange'; 
-judul.style.textAlign = 'center';
-judul.style.background = 'green';
-judul.innerHTML = 'learn Javascript';
+//-- document.getElementById() -> element --
+// const judul = document.getElementById('judul')
+// judul.style.color = 'orange'; 
+// judul.style.textAlign = 'center';
+// judul.style.background = 'green';
+// judul.innerHTML = 'learn Javascript';
 
-// documen.getElementsByTagName() -> HTMLCollection
-const p = document.getElementsByTagName('p');
-p[1].style.background = 'lightgreen'
+// //-- documen.getElementsByTagName() -> HTMLCollection --
+// const p = document.getElementsByTagName('p');
+// p[1].style.background = 'lightgreen'
 
-for( let i = 0; i < 4; i++ ) {
-    p[i].style.backgroundColor = 'lightgreen';
-}
+// for( let i = 0; i < 4; i++ ) {
+//     p[i].style.backgroundColor = 'lightgreen';
+// }
 
-const h1 = document.getElementsByTagName('h1') [0];
-h1.style.color = 'orange';
+// const h1 = document.getElementsByTagName('h1') [0];
+// h1.style.color = 'orange';
 
-// document.getElementsByClassName() -> HTMLCollection
-const p1 = document.getElementsByClassName('p1') [0];
-p1.style.fontSize = '20px';
-p1.innerHTML = 'paragraf pertama';
+// //-- document.getElementsByClassName() -> HTMLCollection --
+// const p1 = document.getElementsByClassName('p1') [0];
+// p1.style.fontSize = '20px';
+// p1.innerHTML = 'paragraf pertama';
 
-// document.querySelector() -> element
-const p4 = document.querySelector('#secdua p');
-p4.style.color = 'yellow';
-p4.innerHTML = 'ini dari javascript'
+// //-- document.querySelector() -> element --
+// const p4 = document.querySelector('#secdua p');
+// p4.style.color = 'yellow';
+// p4.innerHTML = 'ini dari javascript'
 
-const li3 = document.querySelector('section#secdua ul li:nth-child(3)');
-li3.style.background = 'black';
-li3.style.color = 'red';
-li3.style.textAlign = 'center';
+// const li3 = document.querySelector('section#secdua ul li:nth-child(3)');
+// li3.style.background = 'black';
+// li3.style.color = 'red';
+// li3.style.textAlign = 'center';
 
-// document,querySelectorAll() -> Nodelist
+//-- document,querySelectorAll() -> Nodelist --
 // const p = document.querySelectorAll('p');
 // for( let i = 0; i < p.length; i++ ) {
    // p[i].style.fontFamily = 'timesNewRoman';
@@ -41,3 +41,20 @@ li3.style.textAlign = 'center';
 // const sectionsecdua = document.getElementById('secdua');
 // const p4 = sectionsecdua.querySelector('p');
 // p4.style.textAlign = 'center';
+
+//-- MANIPULATION ELEMENT --
+
+//-element.innerHTML-
+const judul = document.getElementById('judul')
+judul.innerHTML = '<em>Learn Javascript</em>';
+
+//-element.syle.propertiCss-
+const a = document.getElementsByTagName('a');
+a[0].style.textDecoration = 'none';
+
+//-set atribute-
+judul.setAttribute('name', 'awal')
+
+//-classList
+document.body.classList.toggle('biru-muda')
+
