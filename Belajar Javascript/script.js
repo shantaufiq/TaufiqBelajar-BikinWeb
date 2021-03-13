688e1e24968eda0c190d778f46b5ab36b14eb1a0
Muhammad Taufiq Shidiq <shantaufiq021@gmail.com>
2021-03-13 09:55:24 +0800

    Belajar javascript 1

---
+++ Belajar Javascript/script.js
@@ -0,0 +1,17 @@
+
+// ! POPUP BOX
+// prompt('masukkan nama:');
+// alert('');
+// confirm(''); -> nilai bolean
+
+alert('selamat datang guys');
+var lagi = true;
+
+while( lagi ) {
+    var nama = prompt('masukkan nama elu:');
+    alert('hello ' + nama);
+
+    lagi = confirm('mau lagi gak?');
+}
+
+alert('terima kasih ..');
